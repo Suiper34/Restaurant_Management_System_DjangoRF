afbb29326dc47dc115b0f85e9ad138ba268f90c4
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-10-20 16:31:56 +0000

    update apps.py and restaurant __init__ to ensure superusers are part of managers group so they could access managers' page and also get db to load at the server's start

--- restaurant/apps.py
+++ restaurant/apps.py
@@ -3,6 +3,20 @@ from __future__ import annotations
 from django.apps import AppConfig
 from django.conf import settings
 from django.contrib import admin
+from django.db.models.signals import post_save
+
+
+def _ensure_superuser_membership(sender, instance, **kwargs) -> None:
+    """
+    Make sure any superuser that gets saved ends up in the Managers group.
+    """
+    if not instance.is_superuser:
+        return
+
+    from django.contrib.auth.models import Group
+
+    managers_group, _ = Group.objects.get_or_create(name='Managers')
+    managers_group.user_set.add(instance)
 
 
 class RestaurantConfig(AppConfig):
@@ -10,6 +24,18 @@ class RestaurantConfig(AppConfig):
     name = 'restaurant'
 
     def ready(self) -> None:
+        from django.contrib.auth import get_user_model
+        from django.contrib.auth.models import Group
+
         admin.site.site_header = settings.ADMIN_SITE_TITLE
         admin.site.index_title = settings.ADMIN_INDEX_TITLE
         admin.site.site_title = settings.ADMIN_SITE_TITLE
+
+        Group.objects.get_or_create(name='Managers')
+
+        User = get_user_model()
+        post_save.connect(
+            _ensure_superuser_membership,
+            sender=User,
+            dispatch_uid='restaurant.ensure_superuser_managers_group',
+        )
